d2d0f2501120eda4c774468d5090c63592bbb998
PULOK AHMED <pulok19524@gmail.com>
2021-08-28 17:25:14 +0600

    update:26/08/2021

--- GeekforGeek/Kadane's Algorithm.cpp
+++ GeekforGeek/Kadane's Algorithm.cpp
@@ -12,42 +12,111 @@ class Solution{
     int maxSubarraySum(int arr[], int n){
 
         // Your code here
-        if(n==1)
-            return arr[0];
 
-        int sum=arr[0];
-        int store[n];
-        int s_sz=0;
-        for(int i=1;i<n;i++){
-
-            if((sum+arr[i])>0){
-                sum+=arr[i];
-            }
-            else{
-                store[s_sz]=sum;
-                s_sz++;
-                sum=arr[i];
-            }
-        }
-                store[s_sz]=sum;
-                s_sz++;
-
-        sum=store[0];
-
-        for(int i=1;i<s_sz;i++){
-            if(store[i]>sum)
-                sum=store[i];
-        }
-
-//        for(int i=0;i<s_sz;i++){
-//            cout<<store[i]<<" ";
+        //Approach:Bruteforces
+//        if(n==1)
+//            return arr[0];
+//
+//            int ans=INT_MIN;
+//
+//            int sum=0;
+//
+//        for(int i=0;i<n;i++){
+//
+//            for(int j=i;j<n;j++){
+//                sum+=arr[j];
+//                if(ans<sum){
+//                    ans=sum;
+//                }
+//
+//            }
+//            sum=0;
 //        }
+//
+//        return ans;
 
-        return sum;
+        //O(n^2)
+
+//Approach 02:Kadane's Algo
+
+//        int maxsum=INT_MIN;
+//        int curmax=0;
+//
+//        for(int i=0;i<n;i++){
+//            curmax+=arr[i];
+//            if(curmax>maxsum)
+//                maxsum=curmax;
+//
+//            if(curmax<0)
+//                curmax=0;
+//        }
+//
+//
+//        return maxsum;
+
+        //O(n)
+
+//Approach:03(change in implementation)
+
+//        int maxsum=arr[0];
+//        int cursum=arr[0];
+//
+//        for(int i=1;i<n;i++){
+//            if((cursum+arr[i])>arr[i]){
+//                cursum+=arr[i];
+//            }
+//            else{
+//                cursum=arr[i];
+//            }
+
+//            if(cursum>maxsum)
+//                maxsum=cursum;
+//        }
+//
+//        return maxsum;
+
+//Approach 04:(ANother simple implementation)
+
+//        int maxsum=arr[0];
+//        int cursum=arr[0];
+//
+//        for(int i=1;i<n;i++){
+//            cursum=max(cursum+arr[i],arr[i]);
+//            maxsum=max(maxsum,cursum);
+//        }
+//
+//        return maxsum;
+   // }
+
+//finding the sub array:
+
+        int maxsum=INT_MIN;
+        int cursum=0;
+        int s=0,e,update=0;
+
+        for(int i=0;i<n;i++){
+
+            cursum+=arr[i];
+
+            if(cursum>maxsum){
+                maxsum=cursum;
+                s=update;
+                e=i;
+            }
+
+            if(cursum<0){
+                cursum=0;
+                update=i+1;
+            }
+
+        }
+
+        cout<<"start: "<<s<<" "<<"end: "<<e<<endl;
+
+        return maxsum;
 
     }
 };
-
 // { Driver Code Starts.
 
 int main()
